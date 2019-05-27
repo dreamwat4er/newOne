4a648b8296d13d8b8ce57b8466986ea009370dc5
Bohdan <dreamwat4er@gmail.com>
2019-05-27 19:36:41 +0300

    second commit

---
+++ yoga/js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function(){
+
+    'use strict';
+    let tab = document.qurySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    
+
+
+
+
+});
